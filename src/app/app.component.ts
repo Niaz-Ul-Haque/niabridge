71c9da6a3f4dbcb9c92b8181c68a308eb9d6bb3e
Niaz Ul Haque <theniaz619.2@gmail.com>
2020-07-14 18:55:20 -0400

    Added some part of menu compo and worked with bridge data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Bridge } from './bridge';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'niabridge';
+
+  currentBridge:Bridge;
+
+  handleBridgeChange(bridge: Bridge): void {
+    console.log("in bridgeChange", bridge);
+    this.currentBridge = bridge;
+  }
 }
